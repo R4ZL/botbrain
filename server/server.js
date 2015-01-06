3b4b24509360a5572d768558d2aff9adb340f286
David Connolly <david@twg.ca>
2015-01-05 22:18:53 -0500

    adding shell plugin and more sample code

--- server/server.js
+++ server/server.js
@@ -4,16 +4,31 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var path = require('path');
+var shell = require('shelljs');
 
 var port = process.env.PORT || 3000;
 
-// Setup and launch server
-app.use(express.static(path.join(__dirname, '..', 'public')));
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+app.post('/send_command', function(req, res){
+  shell.echo(req.query);
+  res.send('success');
 });
 
-http.listen(port, function(){
+http.listen(port, function() {
   console.log('listening on port: ' + port);
+  shell.echo('hello world');
 });
+
+var runSystemJobs = function() {
+  console.log("Running System Jobs")
+}
+
+var runUserJobs = function() {
+  console.log("Running User Jobs")
+  setTimeout(runUserJobs, 1000);
+}
+
+// System Tasks
+setInterval(runSystemJobs, 1000);
+
+// User
+setTimeout(runUserJobs, 1000);
